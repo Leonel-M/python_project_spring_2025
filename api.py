222f5f244174cc4c93027af735085fd7bdb4ba92
Leonel-M <leonel.amarquez92@gmail.com>
2025-05-18 15:06:42 -0500

    refactor: Implement parent class for datasets

--- api.py
+++ api.py
@@ -3,8 +3,29 @@ import json
 # https://stackoverflow.com/questions/66831999/how-to-import-csv-as-a-pandas-dataframe
 import os
 
+class DataFrame:
+    def __init__(self, in_path):
+        self.path = in_path
+        self.df = self.get_data()
+
+    def get_data(self):
+        """
+        Read CSV file
+        :return: DataFrame
+        """
+        df = pd.read_csv(csv_file, encoding='ISO-8859-1')  # alternative encoding with special characters
+        return df
+
+    def get_info(self):
+        """
+        Principal info about DataFrame
+        :return:
+        """
+        print(self.df.info())
+
+
 csv_file = os.path.join('data','superstore_final_dataset (1).csv')
 
-df = pd.read_csv(csv_file, encoding='ISO-8859-1')  # alternative encoding with special characters
 
-print(df.head())
+data = DataFrame(csv_file)
+print(data.df.head())
