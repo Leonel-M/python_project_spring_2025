259b77489982ab149213ab9d01b3721aff3097b9
Leonel-M <leonel.amarquez92@gmail.com>
2025-05-18 14:50:27 -0500

    feat: Kaggle data acquisition using kaggle.api

---
+++ api.py
@@ -0,0 +1,18 @@
+"""
+
+https://github.com/Kaggle/kaggle-api
+"""
+
+import os
+from kaggle.api.kaggle_api_extended import KaggleApi
+import pandas as pd
+
+dataset = 'bhanupratapbiswas/superstore-sales'
+data_dir = 'data'
+
+api = KaggleApi()
+api.authenticate()
+
+print(f'Downloading {dataset}... ')
+api.dataset_download_files(dataset, path=data_dir,unzip=True)
+print(f'Files saved in {data_dir}')
