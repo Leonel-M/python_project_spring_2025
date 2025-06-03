92595cdff011b30cf342dd3eaa07f59c4cd234ad
Leonel-M <leonel.amarquez92@gmail.com>
2025-06-03 14:20:15 -0500

    refactor: Add error handling when downloading and extracting Kaggle datasets

--- api.py
+++ api.py
@@ -23,8 +23,17 @@ dataset = 'bhanupratapbiswas/superstore-sales'
 data_dir = 'data'
 
 api = KaggleApi()
-api.authenticate()
 
-print(f'Downloading {dataset}... ')
-api.dataset_download_files(dataset, path=data_dir,unzip=True)
-print(f'Files saved in {data_dir}')
+try:
+    api.authenticate()
+    print(f'Success authenticate')
+except Exception as e:
+    print(f'Error with authenticate Kaggle API: {e}')
+
+try:
+    print(f'Downloading {dataset}... ')
+    api.dataset_download_files(dataset, path=data_dir,unzip=True)
+    print(f'Files saved in {data_dir}')
+except Exception as e:
+    print(f'Error downloading {dataset}: {e}')
+
